80fe4dc0b6bd94bcc4dbae780b3e80421f1dd5de
Andrei Nevzorov <vzor.mobile@gmail.com>
2022-10-05 11:38:45 +0300

    new file:   Task_1_7_8.c

---
+++ Task_1_7_8.c
@@ -0,0 +1,85 @@
+// Задание 8 по теме 1.7
+// 8. Необходимо создать двумерный массив 5 х 5. Далее написать функцию, которая заполнит его случайными числами от 30 до 60. Создать еще две функции, которые находят максимальный и минимальный элементы этого двумерного массива. 
+
+#include <iostream>
+#include <random>
+#include <iomanip>
+
+
+const int rows = 5;
+const int cols = 5;
+int i,j;
+float array[rows][cols];
+
+float init_array(float (*array)[cols])
+{
+    int i,j;
+    for (i=1; i<rows; i++)
+    {
+        for (j=1; j<cols; j++)
+        {
+           array[i][j]=rand();
+           array[i][j]=array[i][j]/100000-10000;
+        }
+    }
+//    return array;
+}
+float min_a(float (*array)[cols])
+{
+    float min;
+    min = array[1][1];
+    for (i=1; i<rows; i++)
+    {
+        for (j=1; j<cols; j++)
+        {
+           if (array[i][j]<min)
+           {
+               min=array[i][j];
+           }
+        }
+    }
+    return min;
+}
+
+float max_a(float (*array)[cols])
+{
+    float max;
+    max = array[1][1];
+    for (i=1; i<rows; i++)
+    {
+        for (j=1; j<cols; j++)
+        {
+           if (array[i][j]>max)
+           {
+               max=array[i][j];
+           }
+        }
+    }
+    return max;
+}
+
+
+
+int main()
+{
+   std::random_device dev;
+   std::mt19937 rng(dev());
+   std::cout << std::fixed;
+   init_array(array);
+   float min,max;
+    for (i=1; i<rows; i++)
+    {
+        for (j=1; j<cols; j++)
+        {
+
+           std::cout  <<array[i][j] << "	";
+//           std::cout << i << ";" << j << "	";
+        }
+        std::cout << "\n";
+    }
+    min = min_a(array);
+    max = max_a(array);
+    std::cout << "Минимальное значение " << min <<"\n";
+    std::cout << "Максимальное значение " << max <<"\n";    
+   return 0;
+}
